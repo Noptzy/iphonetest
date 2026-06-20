abf6f55755db652dc1a034b54e026039a02abdf7
Noptzy <nopsy.zet@gmail.com>
2026-06-20 23:06:32 +0700

    chore: remove leftover source paths from folder reorg

--- apps/web/src/features/iphones/queries/use-admin-iphones.ts
+++
@@ -1,9 +0,0 @@
-import { type UseQueryResult, useQuery } from "@tanstack/react-query"
-import { type client, orpc } from "@web/libs/orpc/client.ts"
-
-export function useAdminIphones(): UseQueryResult<
-	Awaited<ReturnType<typeof client.iphone.list>>,
-	Error
-> {
-	return useQuery(orpc.iphone.list.queryOptions({ input: {} }))
-}
--- apps/web/src/features/iphones/queries/use-iphone-detail.ts
+++
@@ -1,8 +0,0 @@
-import { type UseQueryResult, useQuery } from "@tanstack/react-query"
-import { type client, orpc } from "@web/libs/orpc/client.ts"
-
-export function useIphoneDetail(
-	id: string,
-): UseQueryResult<Awaited<ReturnType<typeof client.iphone.get>>, Error> {
-	return useQuery(orpc.iphone.get.queryOptions({ input: { id } }))
-}
